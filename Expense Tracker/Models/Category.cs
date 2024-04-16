2c078bbac47f44f903b3f2a74e1f92615b44a04e
Malsha623 <mhsm.dulakshi@gmail.com>
2024-04-16 10:52:00 +0530

    Migrate and update database

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -7,11 +7,11 @@ namespace Expense_Tracker.Models
 	{
 		[Key]
 		public int CategoryId { get; set; }
-		[Column(TypeName = "navarchar(50)")]
+		[Column(TypeName = "nvarchar(50)")]
 		public string Title { get; set; }
-		[Column(TypeName = "navarchar(5)")]
+		[Column(TypeName = "nvarchar(5)")]
 		public string Icon { get; set; } = "";
-		[Column(TypeName = "navarchar(10)")]
+		[Column(TypeName = "nvarchar(10)")]
 		public string Type { get; set; } = "Expense";
 	}
 }
