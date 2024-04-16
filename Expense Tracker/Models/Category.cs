d3b5fe4f691d7ad3d21a4b377e7241d344447adb
Malsha623 <mhsm.dulakshi@gmail.com>
2024-04-16 09:48:18 +0530

    Create Category model

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
+		[Column(TypeName = "navarchar(50)")]
+		public string Title { get; set; }
+		[Column(TypeName = "navarchar(5)")]
+		public string Icon { get; set; }
+		[Column(TypeName = "navarchar(10)")]
+		public string Type { get; set; }
+	}
+}
